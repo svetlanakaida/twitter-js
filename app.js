bb4957478ae8d72c1567e01bef0ba3eda22ca918
svetlanakaida <svetlanakaida@users.noreply.github.com>
2017-06-13 10:59:35 -0400

    nunjucks set

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express(); // creates an instance of an express application
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000...");
+app.listen(3000, function() {
+    console.log("Listening on port 3000...");
 });
 
 app.use(function(req, res, next) {
-  console.log(req.method, req.url);
-  next();
+    console.log(req.method, req.url);
+    next();
 })
 
-app.use("/", function (req, res) {
-  res.send("Welcome!");
-});
+// app.use("/", function(req, res) {
+//     res.send("Welcome!");
+// });
 
+var nunjucks = require('nunjucks');
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+app.use("/", function(req, res) {
+    res.render('index.html', { title: 'Example', people: [{ name: "Gandalf" }, { name: "Frodo" }, { name: "Hermione" }] })
+});
